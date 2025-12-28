a81b76f12004d28b89606dbce31e8c25616bc24e
George Farias <george.farias@webera.com>
2025-12-27 20:53:33 -0400

    Add yaml validation

--- src/devopstoolbox/main.py
+++ src/devopstoolbox/main.py
@@ -1,7 +1,7 @@
 import typer
 from rich import print
 
-from devopstoolbox import generate
+from devopstoolbox import generate, validate
 from devopstoolbox.k8s import certificates, pods, services
 
 __version__ = "DevOpsToolbox v0.1.0"
@@ -9,8 +9,9 @@ __version__ = "DevOpsToolbox v0.1.0"
 app = typer.Typer(no_args_is_help=True)
 k8s_app = typer.Typer(no_args_is_help=True)
 
-app.add_typer(k8s_app, name="k8s")
+app.add_typer(k8s_app, name="k8s", help="Kubernetes utilities")
 app.add_typer(generate.app, name="generate", help="Generate utilities")
+app.add_typer(validate.app, name="validate", help="tools for validation files")
 
 k8s_app.add_typer(pods.app, name="pods", help="Manager Pods")
 k8s_app.add_typer(services.app, name="services", help="Manager Services")
